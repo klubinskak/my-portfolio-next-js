aeceba2d4023f984a9fe7541197fcaf2e0aade20
Klaudia Klubińska <klubinskaklaudia@gmail.com>
2025-01-30 19:38:16 +0100

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 import Head from "next/head";
 import { ThemeWrapper } from "@/components/Layout/theme-wrapper";
+import Script from "next/script";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   
@@ -13,6 +15,23 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Klaudia Klubinska</title>
         <meta name="description" content='Klaudia Klubinska, software developer - my portfolio.' key="desc"/>
         </Head>
+        <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+          `,
+        }}
+      />
         <Component {...pageProps} />
       </ChakraProvider>
     </ThemeWrapper>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <script defer src="https://cloud.umami.is/script.js" data-website-id="4ce9ca7d-1e66-4059-ba51-6953f53dc9a2"></script>
       </Head>
       <body className={ env == "development" ? "debug-screens" : ""}>
         <Main />
